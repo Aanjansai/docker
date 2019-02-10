306332077ea11407acbf6ef6685e88ad0efebd69
Ranjitha-GV <ranjithagowda1024@gmail.com>
2019-02-10 16:30:08 +0530

    updated

---
+++ chatapplication/chat/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+from .views import index, room           # importing views from current directory
+
+app_name = 'chat'
+
+urlpatterns = [
+    path('', index, name="index"),
+    re_path(r'^(?P<room_name>[^/]+)/$', room, name='room'),
+]
