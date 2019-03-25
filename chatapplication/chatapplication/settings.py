859b4176bd16bb736e3726e65a688984f6487fe0
root <root@new-chatapp.us-east1-b.c.web-application-231505.internal>
2019-03-25 16:27:31 +0000

    updated file

--- chatapplication/chatapplication/settings.py
+++ chatapplication/chatapplication/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xux7u0pd8r#k)fbf6+s4!8uksw(e!-y!je($v_j7m1r+g@z32-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'chatapplication',
-        'USER': 'postgres',
+        'USER': 'postgresql',
         'PASSWORD': 'postgresql1112',
         'HOST': '127.0.0.1',
         'PORT': '5432',
@@ -137,4 +137,4 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/chat'
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
