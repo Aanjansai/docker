c057c2ca4a3b0ce281df46bf9b83dfd3a6a58488
aanjansai <aanjan567@gmail.com>
2019-03-25 23:41:34 +0530

    removed myapp

--- myapp/chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path
-from chat.views import index, room           # importing views from current directory
-
-app_name = 'chat'
-
-urlpatterns = [
-    path('', index, name="index"),
-    re_path(r'^(?P<room_name>[^/]+)/$', room, name='room'),
-]
