27aae70887d2366570492bd9b2b78741c6a0323e
aanjansai <aanjan567@gmail.com>
2019-03-28 19:38:47 +0530

    docker entrypoint file

---
+++ docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+python3 manage.py migrate                  # Apply database migrations
+python3 manage.py collectstatic --noinput  # Collect static files
+
+# Prepare log files and start outputting logs to stdout
+touch /srv/logs/gunicorn.log
+touch /srv/logs/access.log
+tail -n 0 -f /srv/logs/*.log &
+
+# start gunicorn processes
+
+echo Starting Gunicorn.
+exec gunicorn chatapplication.wsgi:application \
+    --name chatapplication \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+    --log-level=info \
+    --log-file=/srv/logs/gunicorn.log \
+    --access-logfile=/srv/logs/access.log \
+    "$@"
+
